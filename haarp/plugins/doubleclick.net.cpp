9c3b77241accdfc286eec424b647bde80d8a00e3
Manolo Canales <keikurono01@gmail.com>
2013-04-11 11:40:08 -0500

    Actualizando bloqueo publicidad youtube

--- haarp/plugins/doubleclick.net.cpp
+++ haarp/plugins/doubleclick.net.cpp
@@ -16,7 +16,7 @@ extern "C" resposta hgetmatch2(const string url) {
 	string find2 = "";
 	int len;
 	if(url.find("www.youtube.com") != string::npos) {
-		if ( ( (find = regex_match("[\\?&]ad_type=video($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=text_image_flash($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=text($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=image($|&)",url)) != "" ) && ( (find2 = regex_match("[\\?&]url=http.{2,15}www.youtube.com([a-zA-Z0-9]|%|-|_)*&?",url)) != "" ) ) {
+		if ( ( (find = regex_match("[\\?&]ad_type=skippablevideo($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=video($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=text_image_flash($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=text($|&)",url)) != "" || (find = regex_match("[\\?&]ad_type=image($|&)",url)) != "" ) && ( (find2 = regex_match("[\\?&]url=http.{2,15}www.youtube.com([a-zA-Z0-9]|%|-|_)*&?",url)) != "" ) ) {
 			r.domain = "rewrite";
 			r.file = "googleads.g.doubleclick.net/pagead/ads";
 			r.match = true;
