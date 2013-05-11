e63bc1f918c7326c3e50ac97af5f901340a32fb3
Manolo Canales <keikurono01@gmail.com>
2013-05-10 23:33:06 -0300

    Update speedtest.net.cpp

--- haarp/plugins/speedtest.net.cpp
+++ haarp/plugins/speedtest.net.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 // use this line to compile
 // g++ -I. -fPIC -shared -g -o speedtest.com.so speedtest.com.cpp  
-//~ Ex:
+//~ URL EXP:
 //~ http://speedtest.movistar.com.pe/speedtest/latency.txt?x=1365780932390
 //~ http://speedtest4.claro.com.pe/speedtest/random750x750.jpg?x=1367893686447&y=2
 //~ haarp.lst:
