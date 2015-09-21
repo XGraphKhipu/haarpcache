d3a5e2c0e8008e0fb2f32892c18a6ccc05ba3d12
Kei Kurono <keikurono01@gmail.com>
2015-09-21 14:24:23 -0500

    Installation script HaarpCache

---
+++ installHaarp.sh
@@ -0,0 +1,269 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+#  Copyright 2015, Kei Kurono <kei.haarpcache@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+
+
+echo -e "\e[5m__        __   _                          
+\ \      / /__| | ___ ___  _ __ ___   ___ 
+ \ \ /\ / / _ \ |/ __/ _ \| '_ \` _ \ / _ \\
+  \ V  V /  __/ | (_| (_) | | | | | |  __/
+   \_/\_/ \___|_|\___\___/|_| |_| |_|\___|
+ _ 
+| |_ ___  
+| __/ _ \ 
+| || (_) |
+ \__\___/
+ _   _                         ____           _          
+| | | | __ _  __ _ _ __ _ __  / ___|__ _  ___| |__   ___ 
+| |_| |/ _\` |/ _\` | '__| '_ \| |   / _\` |/ __| \'_ \ / _ \\
+|  _  | (_| | (_| | |  | |_) | |__| (_| | (__| | | |  __/
+|_| |_|\__,_|\__,_|_|  | .__/ \____\__,_|\___|_| |_|\___|
+                       |_|                               \e[25m"
+echo  "Buscando las tarjetas de red ...."
+sleep 0.8
+function getInterfacesIP() {
+	echo "$(ip -o -4 add | egrep -A0 "(eth[0-9]*|enp[0-9]*s[0-9]*)"  | awk '{print $2, $4}' | cut -d '/' -f1)"
+}
+function mask2cidr() {
+    nbits=0
+    IFS=.
+    for dec in $1 ; do
+        case $dec in
+            255) let nbits+=8;;
+            254) let nbits+=7;;
+            252) let nbits+=6;;
+            248) let nbits+=5;;
+            240) let nbits+=4;;
+            224) let nbits+=3;;
+            192) let nbits+=2;;
+            128) let nbits+=1;;
+            0);;
+            *) echo "Error: $dec is not recognised"; exit 1
+        esac
+    done
+    echo "$nbits"
+}
+
+RED='\e[31m';
+NOR='\e[0m';
+#~ 
+interIP=($(getInterfacesIP))
+#~ 
+name=""
+len=${#interIP[@]};
+if [[ $len -eq 0 ]]; then
+	echo -e "${RED}Interfaces no encontrados!, configure sus interfaces!.${NOR}";
+	exit
+fi
+index=0;
+if [ $[len%2] -eq 1 ]; then
+	len=$[len-1];
+fi
+while [[ $name != "yes" ]]; do
+	echo -n -e "${RED}Su red LAN esta en ${interIP[$[index]]} (${interIP[$[index+1]]})${NOR} (yes/no)?: ";
+	read name;
+	index=$[$[index+2]%len];
+done
+sleep 0.5
+#~ 
+ETHLAN=${interIP[$index]};
+IPLAN=${interIP[$[index+1]]};
+#~ 
+NETMASK_LAN="127.0.0.0"
+CIDR_LAN="24"
+#~ 
+network_mask=($(netstat -nr | grep -E " $ETHLAN($| )" | grep -v "^[0.0.0.0]" | awk '{print $1, $3}'))
+if [[ "${#network_mask[@]}" != "2" ]]; then
+	echo ""
+	echo -e "${RED}ADVERTENCIA: network y netmask no configurados para $ETHLAN (${interIP[$[index+1]]})";
+	echo -n -e "${NOR}Pulse enter para continuar ... ";
+	read tmp
+else 
+	NETMASK_LAN=${network_mask[0]}
+	CIDR_LAN=$(mask2cidr ${network_mask[1]})
+fi
+#
+clear
+apt-get update 
+yes | apt-get upgrade
+yes | apt-get install -y build-essential mysql-server mysql-client php5 apache2 php5-mysql libblkid-dev libcurl4-gnutls-dev libmysqlclient15-dev libapache2-mod-auth-mysql libapache2-mod-php5 sharutils curl autoconf squid3 git g++-4.4 bind9 dnsutils
+
+mv /etc/squid3/squid.conf "/etc/squid3/squid.conf.backup_$(date +%Y%m%d)"
+touch /etc/squid3/squid.conf
+echo "#===========================================================#
+#          Generate by the installer of HaarpCache          #
+#===========================================================#
+http_port 3128 intercept
+visible_hostname haarp.cache
+dns_nameservers 8.8.8.8 8.8.4.4
+dns_retransmit_interval 5 seconds
+dns_timeout 2 minutes
+acl localnet src $NETMASK_LAN/$CIDR_LAN # Your LAN
+acl localnet src fc00::/7       # RFC 4193 local private network range
+acl localnet src fe80::/10      # RFC 4291 link-local (directly plugged) machines
+
+acl SSL_ports port 443
+acl Safe_ports port 80          # http
+acl Safe_ports port 21          # ftp
+acl Safe_ports port 443         # https
+acl Safe_ports port 70          # gopher
+acl Safe_ports port 210         # wais
+acl Safe_ports port 1025-65535  # unregistered ports
+acl Safe_ports port 280         # http-mgmt
+acl Safe_ports port 488         # gss-http
+acl Safe_ports port 591         # filemaker
+acl Safe_ports port 777         # multiling http
+acl CONNECT method CONNECT
+
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
+
+http_access allow manager localhost
+http_access deny manager all
+http_access allow localhost
+http_access allow localnet
+#################################
+acl google url_regex -i (googlevideo.com|www.youtube.com)
+acl iphone browser -i regexp (iPhone|iPad)
+acl BB browser -i regexp (BlackBerry|PlayBook)
+acl Winphone browser -i regexp (Windows.*Phone|Trident|IEMobile)
+acl Android browser -i regexp Android
+request_header_access User-Agent deny google !iphone !BB !Winphone !Android
+request_header_replace User-Agent Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+#--- for QUIC connections ---
+reply_header_access Alternate-Protocol deny all
+#################################
+quick_abort_min 0 KB
+quick_abort_max 0 KB
+quick_abort_pct 100
+coredump_dir /var/spool/squid3
+logfile_rotate 5
+access_log /var/log/squid3/access.log
+access_log /var/log/squid3/error.log
+cache_store_log none" > /etc/squid3/squid.conf
+/etc/init.d/squid3 restart
+clear
+echo -e "${RED}*///////////////======= Instalando HaarpCache =======///////////////*${NOR}"
+cd /usr/src/
+git clone https://github.com/keikurono/haarpcache.git
+cd haarpcache
+./configure CXX=g++-4.4
+make
+make install
+echo -e "${RED}Ingrese la contraseña root de Mysql${NOR}"
+mysql -u root -p < haarp2.sql
+yes | cp -u etc/haarp/haarp.php /var/www/  2>/dev/null
+yes | cp -u etc/haarp/haarp.php /var/www/html 2>/dev/null
+cd /etc/init.d
+update-rc.d haarp defaults 98
+echo "#------------- HaarpCache ------------------------
+acl haarp_lst url_regex -i \"/etc/haarp/haarp.lst\"
+cache deny haarp_lst
+cache_peer 127.0.0.1 parent 8080 0 proxy-only no-digest
+dead_peer_timeout 2 seconds
+cache_peer_access 127.0.0.1 allow haarp_lst
+cache_peer_access 127.0.0.1 deny all" >> /etc/squid3/squid.conf
+#
+echo "# HaarpClean: 
+0 */1     * * *   root    /etc/init.d/haarpclean" >> /etc/crontab
+clear
+# block Quic protocol
+iptables -A FORWARD -i $ETHLAN -p udp -m udp --dport 80  -j REJECT --reject-with icmp-port-unreachable
+iptables -A FORWARD -i $ETHLAN -p udp -m udp --dport 443 -j REJECT --reject-with icmp-port-unreachable
+#
+mv /etc/rc.local "/etc/rc.local.backup_$(date +%Y%m%d)"
+echo "#!/bin/sh -e
+iptables -A FORWARD -i $ETHLAN -p udp -m udp --dport 80  -j REJECT --reject-with icmp-port-unreachable
+iptables -A FORWARD -i $ETHLAN -p udp -m udp --dport 44s3 -j REJECT --reject-with icmp-port-unreachable" > /etc/rc.local
+# restart servers
+/etc/init.d/haarp restart
+/etc/init.d/apache2 restart
+squid3 -k reconfigure 
+# DNS configuration
+mv /etc/resolv.conf "/etc/resolv.conf.backup_$(date +%Y%m%d)"
+touch /etc/resolv.conf
+echo "nameserver 8.8.8.8
+nameserver 8.8.4.4" >> /etc/resolv.conf
+mv /etc/bind/named.conf.options "/etc/bind/named.conf.backup_$(date +%Y%m%d)"
+touch /etc/bind/named.conf.options
+echo "options {
+        directory \"/var/cache/bind\";
+        forwarders {
+                8.8.8.8;
+                8.8.4.4;
+        };
+        auth-nxdomain no;
+        listen-on-v6 { any; };
+};" >> /etc/bind/named.conf.options
+
+# Install Haarp-Viewer v1.x
+apt-get install unzip
+mkdir /var/www/html/ 2>/dev/null
+cd /var/www/html/
+wget http://extjs.cachefly.net/ext-3.4.0.zip 
+unzip  ext-3.4.0.zip
+ln -s ext-3.4.0 ext
+ln -s html/ext-3.4.0 ../ext
+
+# Install LibCGI
+cd /usr/src
+apt-get install autoconf git
+git clone git://github.com/keikurono/libcgi.git
+cd libcgi
+./autogen.sh
+./configure --prefix=/usr
+make
+make install    
+
+# Install HaarpViewer
+cd /usr/src/
+git clone git://github.com/keikurono/haarp-viewer.git
+cd haarp-viewer
+cd src/
+make
+
+cd ..
+rm -f /usr/lib/cgi-bin/report.cgi /usr/lib/cgi-bin/haarp.cgi 2>/dev/null
+rm -f /var/www/cgi-bin/report.cgi /var/www/cgi-bin/haarp.cgi 2>/dev/null
+cp src/*.cgi /usr/lib/cgi-bin/ 2>/dev/null
+cp src/*.cgi /var/www/cgi-bin/ 2>/dev/null
+cp hc.html /var/www/html/
+cp hc.html /var/www/ 
+cp -r images /var/www/html/
+cp -r images /var/www/
+touch /var/log/haarp/webaccess.log
+chown www-data:www-data /var/log/haarp/webaccess.log
+echo "ACCESSWEBLOG /var/log/haarp/webaccess.log" >> /etc/haarp/haarp.conf
+
+a2enmod cgi 2>/dev/null
+service apache2 restart 2>/dev/null
+# ----------- iptables for redirect to Squid -----------------
+redirect="iptables -A PREROUTING -t nat -i $ETHLAN -p tcp -m tcp --dport 80  -j REDIRECT --to-ports 3128"
+echo $redirect >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+$redirect
+
+echo -e "        /*       _\|/_
+                 (o o)
+         +----oOO-{_}-OOo------------------------------------------------------------+
+         |                                                                           |
+         | Finish?, access to HaarpViewer: \e[4mhttp:///$IPLAN/cgi-bin/haarp.cgi\e[24m 
+         +--------------------------------------------------------------------------*/"
+
+
