bac20a4a332314a7193dd30fc48a4d20da0d0dc6
Kei Kurono <keikurono01@gmail.com>
2015-12-07 23:25:47 -0500

    Update of the eset plugin.

--- haarp/plugins/eset.com.cpp
+++ haarp/plugins/eset.com.cpp
@@ -5,38 +5,31 @@
 
 // use this line to compile
 // g++ -I. -fPIC -shared -g -o eset.com.so eset.com.cpp
-
+// http.{1,10}update\.eset\.com\/v[0-9]+(.*\/){2}.*\.nup$
+//
 string get_filename(string url) {
-		vector<string> resultado;
-		if (url.find("?") != string::npos) {
-			stringexplode(url, "?", &resultado);
-			stringexplode(resultado.at(resultado.size()-2), "/", &resultado);
-			return resultado.at(resultado.size()-1);           
-		} else {
-			stringexplode(url, "/", &resultado);
-			return resultado.at(resultado.size()-1);
-		}
+	vector<string> split;
+	stringexplode(url, "/", &split);
+	int len = split.size();
+	if ( len >= 3 ) {
+		return split.at(len-3) + DELIM + split.at(len-2) + DELIM + split.at(len-1);
+	} else 
+		return "";
 }
 
-
 extern "C" resposta hgetmatch2(const string url) {
-    resposta r;
+	resposta r;
 	r.range_min = 0;
 	r.range_max = 0;
 		
-	if ( (url.find(".eset.com/") != string::npos) &&
-                (url.find(".nup") != string::npos) && (url.find("update.ver") == string::npos)
-	) {
-		
-	    r.file = get_filename(url);
+	if ( url.find("?") == string::npos ) {
+	    	r.file = get_filename(url);
 		if (!r.file.empty()) {
 			r.match = true;
 			r.domain = "eset";
-		} else {
+		} else 
 			r.match = false;
-		}
-	} else {
+	} else 
 		r.match = false;
-	}
 	return r;
 }
